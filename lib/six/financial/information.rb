debc1b2838b9675453f74e3f3c61b39e705c0679
Volodymyr Shatsky <shockone89@gmail.com>
2014-10-08 14:10:46 +0300

    Initial commit.

---
+++ lib/six/financial/information.rb
@@ -0,0 +1,9 @@
+require "six/financial/information/version"
+
+module Six
+  module Financial
+    module Information
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/six/financial/information/version.rb
@@ -0,0 +1,7 @@
+module Six
+  module Financial
+    module Information
+      VERSION = "0.0.1"
+    end
+  end
+end
