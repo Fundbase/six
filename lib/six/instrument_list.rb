41a9c109b39c7237baf40c1c30ba1ff90ca22e37
AhmedAttyah <kebomix@gmail.com>
2014-10-29 16:00:25 +0200

    RV-333: setup Instrument and InstrumentList

--- lib/six/instrument_list.rb
+++ lib/six/instrument_list.rb
@@ -1,7 +1,15 @@
 module SIX
-  InstrumentList = Struct.new(:data) do
-    def instruments
-      Array.wrap(data['I']).map { |i| Instrument.new(i) }
+  class InstrumentList
+    attr_accessor :instruments
+
+    def initialize(valor, currency, exchange_codes)
+      @instruments = exchange_codes.map do |market|
+        Instrument.new("#{valor},#{market},#{currency}")
+      end
+    end
+
+    def price_request_params
+      @instruments.map.with_index { |instrument, index| ["ik#{index+1}", instrument.value] }.to_h
     end
   end
 end
