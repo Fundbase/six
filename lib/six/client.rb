e68dea9398875b43113da887eca32b1da488f5bb
Matej Chrenko <info@kodujeme.sk>
2016-11-16 13:39:24 +0100

    add try in fetch prices

--- lib/six/client.rb
+++ lib/six/client.rb
@@ -88,7 +88,7 @@ module SIX
       response.errors.each do |error|
         @exceptions << { fund_class_id: nil, message: 'SIX returned an error', status: error['k'], value: error['v'] }
       end if response.errors?
-      SIX::PriceList.new(response['IL']['I'])
+      SIX::PriceList.new(response['IL'].try(:[], 'I'))
     end
 
     private
